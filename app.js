80aee9b1910465ce210531159e82636e822b0620
SaulSilver <hatem.houssein2gmail.com>
2016-11-12 01:36:21 +0100

    start page is done

--- app.js
+++ app.js
@@ -1,36 +1,34 @@
 /**
  * Created by hatem on 2016-11-06.
  */
+"use strict";
 
-var bodyParser = require('body-parser');
-var express = require('express');
-var exphbs = require('express3-handlebars');
-
-var app = express();
+const bodyParser = require('body-parser');
+const express = require('express');
+const exphbs = require('express3-handlebars');
+var scrape = require('./lib/scrape.js');
+const app = express();
 
 // tell express which template engine to use
 app.engine('handlebars', exphbs({ defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
 // tell express where static resources are
 // the index.html is the start page
 app.use(express.static(__dirname + '/public'));
 
-app.set('port', process.env.PORT || 3000);
-
-app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname + '/index.html'));
-});
+app.set('port', process.env.PORT || 5000);
 
 app.listen(app.get('port'), () => {
     console.log('Welcome to http://localhost:' + app.get('port'));
 });
 
-//var startButton = document.getElementById('start_button');
-//startButton.addEventListener('click', startScraping);
-
-
-function startScraping() {
-    let input = document.getElementById('user_url').value;
+app.post('/', (req, res) => {
+    var input = req.body.userUrl;
     console.log(input);
-}
+    scrape.extractLinks(new Array(input));
+});
